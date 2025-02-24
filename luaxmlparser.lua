009a09260153570ee23cc6f2d31098ff3b7ceae5
VulpesDust <noise13qwer@gmail.com>
2025-02-24 17:49:05 +0300

    Update luaxmlparser.lua

--- luaxmlparser.lua
+++ luaxmlparser.lua
@@ -3,7 +3,7 @@ local luaxmlparser = {}
 local TAG = '^([^<]*)<(%/?)([^>]-)(%/?)>'
 
 local function trim(s)
-    local from = s:match"^%s*()"
+    local from = s:match "^%s*()"
     return from > #s and "" or s:match(".*%S", from)
 end
 
