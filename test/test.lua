5d126b37b0b06d01628d739f5ca17f5691bd519a
VulpesDust <noise13qwer@gmail.com>
2025-02-24 15:21:58 +0300

    Create test.lua

---
+++ test/test.lua
@@ -0,0 +1,9 @@
+local luaxmlparser = require('luaxmlparser')
+
+local function readAll(file)
+    local f = assert(io.open(file, "rb"))
+    local content = f:read("*all")
+    f:close()
+    return content
+end
+
